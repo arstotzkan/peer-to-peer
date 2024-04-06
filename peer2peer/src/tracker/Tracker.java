50e7e635722de70d8d4b0c1ee6016ac6801c3313
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2024-04-06 14:03:39 +0300

    initialize tracker stuff
    waiting for memory in order to actually implement all of htis

--- peer2peer/src/tracker/Tracker.java
+++ peer2peer/src/tracker/Tracker.java
@@ -2,16 +2,42 @@ package tracker;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Tracker extends Thread{
 
     ServerSocket server;
+    TrackerMemory memory = new TrackerMemory();
 
     public Tracker(int port) throws IOException {
         this.server = new ServerSocket(port);
     }
 
     public void run(){
-        /*Run server and handle requests*/
+        this.openServer();
+    }
+    public void openServer() {
+        Socket req = null;
+        try {
+            /* Create Server Socket */
+            server = new ServerSocket(60000, 100); //socket for users
+            System.out.println("File processing ready...");
+
+            while (true) {
+                /* Accept the connection */
+                req = server.accept();
+                Thread reqThread = new TrackerRequestHandler(req, this.memory); //TODO: add message from request (somehow)
+                reqThread.start();
+            }
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            try {
+                req.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
     }
 }
--- peer2peer/src/tracker/TrackerRequestHandler.java
+++ peer2peer/src/tracker/TrackerRequestHandler.java
@@ -1,11 +1,74 @@
 package tracker;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class TrackerRequestHandler extends Thread{
     /*WIP, handle requests*/
-    public void handleRegister(){}
-    public void handleLogIn(){}
-    public void handleLogOut(){}
-    public void handleFileUpdate(){}
-    public void handleListRequest(){}
-    public void handleDetailsRequest(){}
+    DataInputStream in;
+    ObjectOutputStream out;
+    String sender;
+    TrackerMemory memory;
+    String message;
+
+    public TrackerRequestHandler(Socket req , TrackerMemory mem) throws IOException {
+        this.out = new ObjectOutputStream(req.getOutputStream());
+        this.in = new DataInputStream(req.getInputStream());
+        this.sender =  req.getRemoteSocketAddress().toString();
+        this.memory = mem;
+    }
+
+    public void run(){
+        try {
+            String message = in.readUTF();
+
+            switch (message){
+                case "register":
+                    this.handleRegister();
+                    break;
+                case "logIn":
+                    this.handleLogIn();
+                    break;
+                case "logOut":
+                    this.handleLogOut();
+                    break;
+//                case "fileUpdate":
+//                    this.handleFileUpdate();
+//                    break;
+                case "listRequest":
+                    this.handleListRequest();
+                    break;
+                case "detailsRequest":
+                    this.handleDetailsRequest();
+                    break;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void handleRegister(){
+        //add user to memory registered users
+        //return confirmation message
+    }
+    public void handleLogIn(){
+        //add user to active users
+        //return confirmation message
+    }
+    public void handleLogOut(){
+        //remove user to active users
+        //return confirmation message
+    }
+//    public void handleFileUpdate(){
+//        //remove user to active users
+//        //return confirmation message
+//    }
+    public void handleListRequest(){
+        //find and return avaiable files from memory
+    }
+    public void handleDetailsRequest(){
+        //get and return details from file
+    }
 }
