3cc3c5fdab65c041f27bacad7c0c709edba16b30
Vasileios.Mvrds <bslmvr@yahoo.gr>
2024-04-14 11:37:53 +0300

    Update tracker/TrackerMemory

--- peer2peer/src/tracker/TrackerMemory.java
+++ peer2peer/src/tracker/TrackerMemory.java
@@ -4,6 +4,7 @@ import models.OnlineUser;
 import models.UploadedFile;
 import models.User;
 
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 
@@ -12,13 +13,117 @@ public class TrackerMemory {
     ConcurrentLinkedQueue<OnlineUser> loggedInUsers = new ConcurrentLinkedQueue<OnlineUser>();
     ConcurrentLinkedQueue<UploadedFile> fileNames = new ConcurrentLinkedQueue<UploadedFile>();
 
-    /*
-    TODO:
-        -getter method for each linked queue
-        -methods that returns an array list the names of all UploadedFile/Users/OnlineUsers
-        -methods that returns details of a specific User/OnlineUser/UploadedFile (input: its name, output: corresdponding object)
-        -methods to add a User/OnlineUser/UploadedFile into each of the ConcurrentLinkedQueues (input: object)
-        -methods to remove a User/OnlineUser/UploadedFile from each of the ConcurrentLinkedQueues (input: its name)
-        -methods to increase countDownloads, countFailures for a specific user (input:name)
-    * */
+    
+
+    public ConcurrentLinkedQueue<User> getUsers(){
+        return users;
+    }
+
+    public ConcurrentLinkedQueue<OnlineUser> getLoggedInUsers(){
+        return loggedInUsers;
+    }
+
+    public ConcurrentLinkedQueue<UploadedFile> getFileNames(){
+        return fileNames;
+    }
+
+    /*μέθοδος που δέχεται το username και ψάχνει στη λίστα των χρηστών και αν 
+     * βρει το χρήστη τον επιστρέφει αν όχι επιστρέφει null
+     */
+    public User getUser(String username){
+        for (User u: users){
+            if (u.getUser_name().equals(username)){
+                return u;
+            }
+        }
+        return null;
+    }
+
+    /*μέθοδος που δέχεται το username και ψάχνει στη λίστα των online χρηστών και αν 
+     * βρει το χρήστη τον επιστρέφει αν όχι επιστρέφει null
+     */
+    public OnlineUser getOnlineUser(String username){
+        for (OnlineUser u: loggedInUsers){
+            if (u.getUser_name().equals(username)){
+                return u;
+            }
+        }
+        return null;
+    }
+
+    /*μέθοδος που δέχεται το όνομα αρχείου και ψάχνει στη λίστα των αρχείων και αν 
+     * το βρει το επιστρέφει αν όχι επιστρέφει null
+     */
+    public UploadedFile getUploadedFile(String file1){
+        for (UploadedFile f: fileNames){
+            if (f.getName().equals(file1)){
+                return f;
+            }
+        }
+        return null;
+    }
+
+    /*μέθοδος για προσθήκη χρήστη στην ουρά */
+    public void addUser(User user1){
+        users.add(user1);
+    }
+
+    /*μέθοδος για προσθήκη online χρήστη στην ουρά */
+    public void addOnlineUser(OnlineUser user1){
+        loggedInUsers.add(user1);
+    }
+
+    /*μέθοδος για προσθήκη αρχείου στην ουρά */
+    public void addUploadedFile(UploadedFile file1){
+        fileNames.add(file1);
+    }
+    /*μέθοδος για αφαίρεση χρήστη στην ουρά */
+    public boolean removeUser(String username){
+        for(Iterator<User> it = users.iterator(); it.hasNext(); ) {
+            User u = it.next();
+            if (u.getUser_name().equals(username)){
+                it.remove();
+                return true;
+            }
+      }
+      return false;
+    }
+    /*μέθοδος για αφαίρεση online χρήστη στην ουρά */
+    public boolean removeOnlineUser(String username){
+        for(Iterator<OnlineUser> it = loggedInUsers.iterator(); it.hasNext(); ) {
+            OnlineUser u = it.next();
+            if (u.getUser_name().equals(username)){
+                it.remove();
+                return true;
+            }
+      }
+      return false;
+    }
+    /*μέθοδος για αφαίρεση αρχείου στην ουρά */
+    public boolean removeUploadedFile(String file1){
+        for(Iterator<UploadedFile> it = fileNames.iterator(); it.hasNext(); ) {
+            UploadedFile f = it.next();
+            if (f.getName().equals(file1)){
+                it.remove();
+                return true;
+            }
+      }
+      return false;
+    }
+    /*μέθοδος για αύξηση του count downloads για συγκεκριμένο χρήστη */
+    public void increaseCountDownloads(String username){
+        for (OnlineUser u: loggedInUsers){
+            if (u.getUser_name().equals(username)){
+                u.increaseCount_downloads();
+            }
+        }
+    }
+    /*μέθοδος για αύξηση του count failures για συγκεκριμένο χρήστη */
+    public void increaseCountFailures(String username){
+        for (OnlineUser u: loggedInUsers){
+            if (u.getUser_name().equals(username)){
+                u.increaseCount_failures();;
+            }
+        }
+    }
 }
