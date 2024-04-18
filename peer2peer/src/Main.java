68a7831ed69d138c3f7c8e2b9afce9124e64a624
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2024-04-18 16:35:01 +0300

    refactor

--- peer2peer/src/Main.java
+++ peer2peer/src/Main.java
@@ -1,16 +1,85 @@
-import peer.CreatePeers;
-import tracker.TrackerInit;
+import peer.Peer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    /*maybe setting up everything here will be useful for development*/
-    public static void main(String[] args) {
-        try{
-            String[] a = {"127.0.0.1"};
-            TrackerInit.main();
-            CreatePeers.main(a);
-        } catch (Exception e){
-            e.printStackTrace();
+    static Scanner scanner = new Scanner(System.in);
+    static String trackerAddress = "localhost";
+    static Peer peer;
+
+    static String sharedDir;
+    public static void main(String[] args) throws IOException {
+        sharedDir = args[0];
+        trackerAddress = args[1];
+        logInOrRegisterScreen();
+    }
+
+    private static void logInOrRegisterScreen() throws IOException {
+        String choice = "";
+        while (!choice.equals("0") && !choice.equals("1") && !choice.equals("2") ) {
+            System.out.println("1)Log in\n2)Register\n0)Go back");
+            choice = scanner.nextLine();
         }
+
+        if (choice.equals("1")){
+            logInScreen();
+        } else if (choice.equals("2")) {
+            registerScreen();
+        }
+    }
+
+    private static void logInScreen() throws IOException {
+        System.out.println("\n\nUsername: ");
+        String username = scanner.nextLine();
+        System.out.println("\nPassword: ");
+        String password = scanner.nextLine();
+
+        peer = new Peer(username,password, sharedDir,trackerAddress );
+
+        String loginMessage = peer.logIn();
+        System.out.println(loginMessage);
+
+        if (loginMessage.contains("Login successful"))
+            mainScreen();
+        else
+            logInOrRegisterScreen();
+    }
+    private static void registerScreen() throws IOException {
+        System.out.println("\n\nUsername: ");
+        String username = scanner.nextLine();
+        System.out.println("\nPassword: ");
+        String password = scanner.nextLine();
+
+        peer = new Peer(username,password, sharedDir,trackerAddress);
+        String registerMessage = peer.register();
+        System.out.println("\n" + registerMessage);
+
+        if (registerMessage.contains("Registration successful"))
+            mainScreen();
+        else
+            logInOrRegisterScreen();
+    }
+
+    private static void mainScreen(){
+        ArrayList<String> fileList = peer.list();
+
+        if (fileList.size() > 0){
+            System.out.println("\n\n" + fileList);
+            System.out.println("\nSelect filename to download: ");
+            String filename = scanner.nextLine();
+
+            downloadFileScreen(filename);
+        } else {
+            mainScreen();
+        }
+    }
+
+    private static void downloadFileScreen(String filename){
+        String downloadMessage = peer.downloadFile(filename);
+        System.out.println("\n\n" + downloadMessage);
+        mainScreen();
 
     }
 }
---
+++ peer2peer/src/SetUp.java
@@ -0,0 +1,16 @@
+import peer.CreatePeers;
+import tracker.TrackerInit;
+
+public class SetUp {
+    /*maybe setting up everything here will be useful for development*/
+    public static void main(String[] args) {
+        try{
+            String[] a = {"127.0.0.1"};
+            TrackerInit.main();
+            CreatePeers.main(a);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
