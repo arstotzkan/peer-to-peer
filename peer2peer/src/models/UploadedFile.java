5f1d3c25653d8cf60620ddfe94c67d9537b1038d
proff-nvm <proffkhalifa@hotmail.com>
2024-05-26 15:53:34 +0300

    download routine( m bgike o patos)

--- peer2peer/src/models/UploadedFile.java
+++ peer2peer/src/models/UploadedFile.java
@@ -2,42 +2,55 @@ package models;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UploadedFile implements Serializable {
-    
+    private String name;
+    private int fragmentCount;
+    private ArrayList<OnlineUser> usersWithFile;
 
-     String name;
-     ArrayList<OnlineUser> usersWithFile;
-
-     public UploadedFile(){
+    public UploadedFile() {
         usersWithFile = new ArrayList<>();
-        name="";
-     }
+        name = "";
+    }
 
-     public UploadedFile(String name){
+    public UploadedFile(String name) {
         usersWithFile = new ArrayList<>();
-        this.name=name;
-     }
+        this.name = name;
+    }
 
-     public ArrayList<OnlineUser> getUsersWithFile(){
+    public UploadedFile(String name, int fragmentCount, ArrayList<OnlineUser> usersWithFile) {
+        this.name = name;
+        this.fragmentCount = fragmentCount;
+        this.usersWithFile = usersWithFile;
+    }
+
+    public ArrayList<OnlineUser> getUsersWithFile() {
         return usersWithFile;
-     }
+    }
 
-     public boolean userHasFile(String username){
-         for (OnlineUser ou: usersWithFile){
-             if (ou.getUsername().equals(username)){
-                 return true;
-             }
-         }
+    public boolean userHasFile(String username) {
+        for (OnlineUser ou : usersWithFile) {
+            if (ou.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-         return false;
-     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-     public void setName(String name){
-        this.name=name;
-     }
-
-     public String getName(){
+    public String getName() {
         return name;
-     }
+    }
+
+    public int getFragmentCount() {
+        return fragmentCount;
+    }
+
+    public void setFragmentCount(int fragmentCount) {
+        this.fragmentCount = fragmentCount;
+    }
 }
